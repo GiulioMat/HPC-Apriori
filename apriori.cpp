33756941eb49b4a638c1fdccd781583ef7c2e07b
GiulioMat <giulio.mattolin@studenti.unitn.it>
2021-11-21 17:56:33 +0100

    Add function read_file and insert into map

--- apriori.cpp
+++ apriori.cpp
@@ -5,10 +5,48 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <map>
 using namespace std;
 
+vector< vector<string> > read_file(char file_name[]);
+
+// ------------------------------------------------------------
+// Main
+// ------------------------------------------------------------
+
 int main (){
-    ifstream myfile ("../prova.txt");
+    char file_name[] = "./prova.txt";
+    vector< vector<string> > matrix;
+    map<string,int> dictionary;
+
+    matrix = read_file(file_name);
+
+    for (int i = 0; i < matrix.size(); i++){
+        for (int j = 0; j < matrix[i].size(); j++){
+            string item = matrix[i][j];
+
+            if(dictionary.count(item)){ // if key exists
+                dictionary[item]++;
+            }
+            else{
+                dictionary.insert(pair<string,int>(item, 1));
+            }
+        }
+    }
+
+    for(map<string, int>::iterator it=dictionary.begin();it!=dictionary.end();it++) {
+        cout<<it->first<<" "<<it->second<<endl;
+    }
+
+    return 0;
+}
+
+// ------------------------------------------------------------
+// Functions
+// ------------------------------------------------------------
+
+vector< vector<string> > read_file(char file_name[]){
+    ifstream myfile (file_name);
 
     vector< vector<string> > matrix;
     vector<string> row;  
@@ -30,17 +68,7 @@ int main (){
         row.clear();
     }
 
-
     myfile.close();
-    
 
-    for (int i = 0; i < matrix.size(); i++){
-        for (int j = 0; j < matrix[i].size(); j++){
-            cout << matrix[i][j]<<' ';
-        }
-        cout << "\n";
-    }
-
-
-    return 0;
+    return matrix;
 }
